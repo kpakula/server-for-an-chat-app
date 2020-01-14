790614ef059213e63adc93816b1d378bce6f6927
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-14 12:06:49 +0100

    Created get user in room

--- users.js
+++ users.js
@@ -25,10 +25,6 @@ const removeUser = (id) => {
     }
 }
 
-const getUser = () => {
+const getUser = (id) => users.find((user) => user.id === id);
 
-}
-
-const getUserInRoom = () => {
-
-}
+const getUserInRoom = (room) => users.filter((user) = user.room === room);
