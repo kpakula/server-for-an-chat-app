4fc1024b0af41702c78ddd1347679777d4b91987
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-15 19:37:32 +0100

    Added send message in room

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ io.on('connection', (socket) => {
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`})
 
         socket.join(user.room);
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message});
+
+        callback();
     })
 
     socket.on('disconnect', () => {
